ddfc4d4d7c7b05102ad9fa863a7035bf87893b11
davidmalcat <david>
2023-01-27 18:01:28 +0100

    initial commit

---
+++ router.php
@@ -0,0 +1,109 @@
+<?php
+//hezké URL - funkčnost zajišťuje .htaccess
+
+$route = $_GET['route'];
+
+    if (preg_match('/o-nas/', $route)):
+    require('o-nas.php');      
+    
+    
+    elseif (preg_match('/novinky/', $route)):
+    require('novinky.php');   
+    
+    //detail novinky
+    elseif (preg_match('/novinka/', $route)):
+    require('novinka.php');
+    
+    //detail spolupráce
+    elseif (preg_match('/spoluprace/', $route)):
+    require('spoluprace.php');
+
+    elseif (preg_match('/grand-prix-architektu/', $route)):
+        require('gpa.php');
+
+    elseif (preg_match('/grand-prix-architekt-2021/', $route)):
+        require('gpa-detail-2021.php');
+
+    elseif (preg_match('/grand-prix-architekt-2022/', $route)):
+        require('gpa-detail-2022.php');
+       
+    elseif (preg_match('/grand-prix-festival/', $route)): 
+    require('grand-prix-festival.php');  
+    
+    elseif (preg_match('/clenstvi/', $route)): 
+        require('clenstvi.php');
+
+    elseif (preg_match('/uia/', $route)):
+        require('./uia.php');
+    
+    elseif (preg_match('/kontakty/', $route)):
+        require('./kontakty.php');
+
+    elseif (preg_match('/galerie-galavacer-predavani/', $route)):
+        require('./components/galleries/galavecer.php');
+
+    elseif (preg_match('/galerie-galavacer-neformalni/', $route)):
+        require('./components/galleries/neformalni.php');
+
+    elseif (preg_match('/galerie-porota/', $route)):
+        require('./components/galleries/porota.php');
+
+    elseif (preg_match('/galerie-vernisaz/', $route)):
+        require('./components/galleries/vernisaz.php');
+
+    elseif (preg_match('/galerie-video/', $route)):
+        require('./components/galleries/video.php');
+
+    elseif (preg_match('/galerie-galavacer-2022/', $route)):
+        require('./components/galleries/2022/galavecer.php');
+
+    elseif (preg_match('/galerie-festival-2022/', $route)):
+        require('./components/galleries/2022/festival.php');
+    
+    elseif (preg_match('/galerie-galavecer-2022-joze-plecinka-spanelsky-sal/', $route)):
+            require('./components/galleries/2022/joze-plecinka-spanelsky-sal.php');
+    
+    elseif (preg_match('/galerie-2022-manes-tiskova-konference/', $route)):
+        require('./components/galleries/2022/manes-konference.php');
+
+    elseif (preg_match('/galerie-2022-krest-alfabet/', $route)):
+        require('./components/galleries/2022/krest-alfabet.php');
+
+    elseif (preg_match('/galerie-2022-predavani-cen-joze-plecinka-s-prezidentem/', $route)):
+        require('./components/galleries/2022/joze-plecinka-prezident.php');
+    
+    elseif (preg_match('/galerie-2022-robert-konieczny-prednaska/', $route)):
+        require('./components/galleries/2022/robert-konieczny-prednaska.php');
+
+    elseif (preg_match('/galerie-2022-porota-v-praze/', $route)):
+        require('./components/galleries/2022/porota.php');
+
+    // PDFs
+    elseif (preg_match('/katalog-pdf/', $route)):
+        require('./components/downloads/catalog-pdf.php');
+
+    elseif (preg_match('/vysledky-pdf/', $route)):
+        require('./components/downloads/vysledky-pdf.php');
+
+    elseif (preg_match('/shortlist-pdf/', $route)):
+        require('./components/downloads/shortlist-pdf.php');
+
+    elseif (preg_match('/projekty-pdf/', $route)):
+        require('./components/downloads/projekty-pdf.php');
+
+    elseif (preg_match('/vyhlaseni-rocniku-2022/', $route)):
+        require('./components/downloads/2022/vyhlaseni-rocniku.php');
+
+    elseif (preg_match('/prodlouzeni-terminu-prihlaseni-do-souteze-2022/', $route)):
+        require('./components/downloads/2022/prodlouzeni-terminu-prihlaseni-do-souteze.php');
+
+    elseif (preg_match('/shortlist-2022-pdf/', $route)):
+        require('./components/downloads/2022/shortlist.php');
+
+    elseif (preg_match('/vysledky-2022-pdf/', $route)):
+        require('./components/downloads/2022/vysledky-pdf.php');
+
+
+    endif;
+?>    
+    
---
+++ uia.php
@@ -0,0 +1,76 @@
+<?php include ('config.php');?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>UIA 2021 | Grand Prix Architektů</title>
+
+	<?php include('./components/head.php'); ?>
+</head>
+<body>
+
+<!-- Menu -->
+<?php include ('menu.php');?>
+
+<section class="gpa">
+	<div class="gpa__wrapper wrapper">
+		
+		<h1>Mezinárodní unie architektů</h1>
+
+		<div class="gpa__row">
+			<div class="gpa__col">
+				<p>
+                    UIA byla založena 28.&nbsp;června 1948 ve&nbsp;švýcarském Lausanne po&nbsp;sloučení Comité permanent international des Architectes a&nbsp;Réunion International des Architectes. Československo bylo zakládajícím členem. Členství České republiky bylo obnoveno v&nbsp;roce 2017.
+				</p>
+				<p>
+                    Organizace je&nbsp;uznávána jako jediná globální architektonická organizace většinou agentur OSN, včetně UNESCO, UNCHS, ECOSOC, UNIDO a&nbsp;Světové zdravotnické organizace.
+				</p>
+				<p>
+                    Její sídlo se&nbsp;nachází v&nbsp;Paříži.  
+    			</p>
+				<p>
+                    Prostřednictvím svých členských sekcí je&nbsp;UIA zastoupena ve&nbsp;více než 100 státech a&nbsp;územích, geograficky seskupených do&nbsp;pěti regionů.
+                </p>
+			</div>
+			<div class="gpa__col">
+				<p>
+                    Současný prezident: José Luis Cortés - Mexiko (2021-2023)
+				</p>
+				
+				<p>
+                    Česká republika je&nbsp;součástí Regionu II.  a&nbsp;má 2 hlasy.
+                </p>
+
+                <p>
+                    Členové Obce architektů zasedají ve&nbsp;3 komisích – Architectural Education Commission, Professional Practice Commission, Sustainable Development Goals Commission a&nbsp;tím bylo znovu vykročeno k&nbsp;zajištění ochrany a&nbsp;práv českých architektů ve&nbsp;světě.
+                </p>
+
+                <p>
+                    Obec architektů podává návrhy na&nbsp;nominace prestižních světových cen UIA:
+                </p>
+
+                <ul>
+                    <li>Gold Medal</li>
+                    <li>Auguste Perret Prize (Karel Hubáček 1969)</li>
+                    <li>Jean Tschumi Prize</li>
+                    <li>Robert Matthew Prize</li>
+                    <li>Vassilis Sgoutas Prize</li>
+                </ul>
+			</div>
+            <div class="gpa__col">
+				<img class="gpa__logo" src="<?php echo $assetsPath ?>assets/images/uia.png" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="footer" class="footer">
+	<?php include('footer.php'); ?>
+</section>
